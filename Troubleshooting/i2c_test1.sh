1169da4e693ee2402b9d7fa5bbd7416f1ad63be8
cleoqc <cleoqc1124@gmail.com>
2016-12-22 18:02:02 +0000

    add a comment to justify the sleep 1 for our future selves' sanity

--- Troubleshooting/i2c_test1.sh
+++ Troubleshooting/i2c_test1.sh
@@ -4,6 +4,10 @@ echo Checking I2C bus for devices
 echo ============================
 echo ""
 
+# insert a delay here so we know for sure that the Atmega is ready
+echo "Please wait a second..."
+sleep 1
+
 echo Checking I2C bus 0
 echo ==================
 i2cdetect -y 0 
